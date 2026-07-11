b56d7f988fce5a1cfcff8b175837731e09474acf
JHB <16675200+jharveyb@users.noreply.github.com>
2026-07-10 20:32:34 -0400

    fuzz: skip CLMUL_TRI properly

--- src/fuzz/decode.cpp
+++ src/fuzz/decode.cpp
@@ -27,7 +27,7 @@ std::vector<Minisketch> CreateAll(uint32_t bits, size_t capacity) {
     std::vector<Minisketch> ret;
     uint32_t max_impl = Minisketch::MaxImplementation();
     // TODO: temporary clamp to ignore CLMUL_TRI for now
-    uint32_t max_applied_impl = std::max(max_impl, (uint32_t)1);
+    uint32_t max_applied_impl = std::min(max_impl, (uint32_t)1);
     for (uint32_t impl = 0; impl <= max_applied_impl; ++impl) {
         if (Minisketch::ImplementationSupported(bits, impl)) {
             ret.push_back(Minisketch(bits, impl, capacity));
